7f9700edd53de182e016dde8f2b05c9a9f29fb22
HARINI DEVI D <144649767+HarinideviD@users.noreply.github.com>
2025-06-03 12:10:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,20 +29,7 @@ if (typeof Typed !== "undefined") {
     loop: true
   });
 }
-var tablinks= document.getElementsByClassName("tab-links");
-  var tabcontents = document.getElementsByClassName("tab-contents");
 
-  function opentab(tabname){
-    for(tablink of tablinks){
-      tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-      tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab");
-
-  }
 
   document.querySelectorAll('.like-btn').forEach(btn => {
     btn.addEventListener('click', () => {
